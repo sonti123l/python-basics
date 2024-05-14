a3fc9e74da24ba8f55da9fdf9a7a2f1f8c97a5a6
sonti123l <116090293+sonti123l@users.noreply.github.com>
2024-05-14 10:02:52 +0530

    Update tip.py

---
+++ PS0/tip.py
@@ -0,0 +1,18 @@
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent/100
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(d):
+    new_value = d.split("$")
+    return float(new_value[1])
+
+
+def percent_to_float(p):
+    new_value = p.split("%")
+    return int(new_value[0])
+
+
+main()
